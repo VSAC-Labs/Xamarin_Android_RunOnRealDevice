9c038b1398c87765bccea1f4377a1d6a85200c6f
tdevere <tdevere@microsoft.com>
2023-03-03 19:31:17 -0600

    updated script

--- Xamarin_Android_RunOnRealDevice/appcenter-post-build.sh
+++ Xamarin_Android_RunOnRealDevice/appcenter-post-build.sh
@@ -13,7 +13,7 @@ echo "APPPATH " $APPPATH
 echo "BUILDDIR " $BUILDDIR
 echo "TESTSERIES " $TESTSERIES
 echo "DEVICESET " $DEVICESET
-echo "App Center App Name ${APP}"
+echo "App Center App Name " $APP
 echo "TestParamater " $TestParamater
 echo "GITCLONEURLUITEST " $GITCLONEURLUITEST
 echo "UITestDirectory " $UITestDirectory
@@ -25,11 +25,15 @@ pwd
 echo "clone " $GITCLONEURLUITEST
 git clone $GITCLONEURLUITEST
 
-echo "Checout ${UITESTBranch}"
+echo "ls directory"
+pwd
+ls
+
+echo "Checout " ${UITESTBranch
 git branch --all
 git checkout $UITESTBranch
 
-echo "Copy UITest files ${$UITestDirectory} into the build directory ${BUILDDIR}"
+echo "Copy UITest files " $UITestDirectory " into the build directory " $BUILDDIR
 cp -r $UITestDirectory $BUILDDIR
 
 echo "AppCenter Login"
