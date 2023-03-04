0608de0a0e14d9986650585de52fba0f37069e7a
tdevere <tdevere@microsoft.com>
2023-03-03 19:22:40 -0600

    updated script

--- Xamarin_Android_RunOnRealDevice/appcenter-post-build.sh
+++ Xamarin_Android_RunOnRealDevice/appcenter-post-build.sh
@@ -11,16 +11,13 @@ fi
 echo "List Varibles"
 echo "APPPATH " $APPPATH
 echo "BUILDDIR " $BUILDDIR
-echo "BUILDSCRIPT " $BUILDSCRIPT
-echo "MANIFEST " $MANIFEST
 echo "TESTSERIES " $TESTSERIES
 echo "DEVICESET " $DEVICESET
-echo "APP " $APP
+echo "App Center App Name ${APP}"
 echo "TestParamater " $TestParamater
-echo "UITestProject " $UITestProject
-echo "UITestTools " $UITestTools
-echo "UITESTBranch " $UITESTBranch
 echo "GITCLONEURLUITEST " $GITCLONEURLUITEST
+echo "UITestDirectory " $UITestDirectory
+echo "UITESTBranch " $UITESTBranch
 
 echo "CURRENT FOLDER"
 pwd
@@ -28,22 +25,17 @@ pwd
 echo "clone " $GITCLONEURLUITEST
 git clone $GITCLONEURLUITEST
 
-echo "switch to UITEST branch "$UITESTBranch
+echo "Checout ${UITESTBranch}"
 git branch --all
 git checkout $UITESTBranch
 
-echo "Nuget Restore"
-cd AppCenter_Generated_UITest_Base
-pwd
-nuget restore
+echo "Copy UITest files ${$UITestDirectory} into the build directory ${BUILDDIR}"
+cp -r $UITestDirectory $BUILDDIR
 
 echo "AppCenter Login"
 appcenter login --token $AppCenterTokenForTest
 
-echo "Copy UITest files into the build directory "$BUILDDIR
-cp -r /Users/runner/work/1/s/AppCenterXamrain_Test_Example/UITEST_Base $BUILDDIR
-
 echo "appcenter test run uitest"
-appcenter test run uitest --app-path $APPPATH --app $APP --devices $DEVICESET --test-series $TESTSERIES --locale en_US -p $TestParamater --build-dir $UITestProject --uitest-tools-dir $UITestTools --debug --quiet --token $AppCenterTokenForTest
+appcenter test run uitest --app-path $APPPATH --app $APP --devices $DEVICESET --test-series $TESTSERIES --locale en_US -p $TestParamater --build-dir $UITestProject --uitest-tools-dir $UITestDirectory --debug --quiet --token $AppCenterTokenForTest
 
 echo "RunOnRealDevice Complete"
